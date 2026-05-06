5f73803ce01fe9859362c7065ba11e4afe9ed471
CORTINAS GARCIA Elena <elena.cortinasgarcia@etu.toulouse-inp.fr>
2026-05-06 16:48:24 +0200

    carte en 3D faite

--- src/Carte.java
+++ src/Carte.java
@@ -7,25 +7,78 @@ import java.util.List;
  */
 public class Carte {
     // Attributs privés selon le diagramme UML
-    private Case[][] matrice;
+    private Case[][][] matrice;
     private int largeur;
+    private int longueur
     private int hauteur;
 
     /**
      * Constructeur pour initialiser une carte de taille donnée
      * @param largeur Nombre de cases en X
-     * @param hauteur Nombre de cases en Y
+     * @param longueur Nombre de cases en Y
+     * @param hauteur Nombre de cases en Z
      */
-    public Carte(int largeur, int hauteur) {
+    public Carte(int largeur, int longueur, int hauteur) {
         this.largeur = largeur;
+        this.longueur = longueur;
         this.hauteur = hauteur;
-        this.matrice = new Case[largeur][hauteur];
+        this.matrice = new Case[largeur][longueur][hauteur];
 
         // Initialisation de chaque case de la matrice
         for (int i = 0; i < largeur; i++) {
-            for (int j = 0; j > hauteur; j++) {
-                this.matrice[i][j] = new Case();
+            for (int j = 0; j > longueur; j++) {
+                for (int k = 0; k > hauteur; k++) {
+                    this.matrice[i][j][k] = new Case();
+                }
             }
         }
     }
+
+    /**
+     * Retourne la case située aux coordonnées (x, y, z)
+     * @return La case correspondante ou null si hors limites
+     */
+    public Case getTile(int x, int y, int z) {
+        if (estDansLimites(x, y, z)) {
+            return matrice[x][y][z];
+        }
+        return null;
+    }
+
+    /**
+     * Vérifie si les coordonnées fournies à l'intérieur de la carte
+     */
+    public boolean estDansLimites(int x, int y, int z) {
+        return (x >= 0 && x < largeur && y >= 0 && y < longueur && z >= 0 && z < longueur);
+    }
+
+    /**
+     * Retourne la liste des cases adjacentes à la position donnée (N, S, E, O, au-dessus, en-dessous)
+     * Utile pour la détection de gisements voisins ou les déplacements
+     */
+    public List<Case> getVoisins(int x, int y, int z) {
+        List<Case> voisins = new ArrayList<>();
+
+        // Coordonnées des voisins potentiels (Nord, Sud, Est, Ouest, Au-dessus, En-dessous)
+        int [] directions = {{0, 1, 0}, {0, -1, 0}, {1, 0, 0}, {-1, 0, 0}, {0, 0, 1}, {0, 0, -1}};
+
+        for (int[] dir : directions) {
+            int nx = x + dir[0];
+            int ny = y + dir[1];
+            int nz = z + dir[2];
+
+            if (estDansLimites(nx, ny, nz)) {
+                voisins.add(matrice[nx][ny][nz]);
+            }
+        }
+
+        return voisins;
+    }
+
+    // Getters pour la taille
+    public int getLargeur() { return largeur; }
+    public int getLongueur() { return longueur; }
+    public int getHauteur() { return hauteur; }
+
+
 }
