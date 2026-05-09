6ae9a12ee37cd7d35e32ae139d1350b2bcb4b921
Marie Subra <mariesubra0933@gmail.com>
2026-05-09 09:16:02 +0200

    Création type bâtuments

--- src/ressources/Ouvrier.java
+++ src/ressources/Ouvrier.java
@@ -1,4 +1,4 @@
-package code;
+package ressources;
 
 /**
  * Représente un travailleur de la colonie.
---
+++ src/ressources/TypeBatiments.java
@@ -0,0 +1,17 @@
+package ressources;
+
+public enum TypeBatiment {
+
+    // Lieux de ressources
+    FORET,
+    MINE,
+    CARRIERE,
+    PUITS_PETROLE,
+
+    // Usines
+    MENUISERIE,
+    FONDERIE,
+    RAFFINERIE,
+    USINE_ELECTRONIQUE,
+    ZONE_ASSEMBLAGE
+}
